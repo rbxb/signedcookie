238403e9d024ae237fd4fd7d3472d4a8a4aa87b3
ryan <26235793+rbxb@users.noreply.github.com>
2020-12-09 14:08:24 -0600

    Update signedcookie_test.go

--- signedcookie_test.go
+++ signedcookie_test.go
@@ -1,28 +1,47 @@
 package signedcookie
 
 import (
+	"crypto/rand"
 	"crypto/sha256"
+	"encoding/base64"
 	"testing"
 )
 
-func TestVerify(t *testing.T) {
-	text := "hello"
-	b := DefaultSigner.sign([]byte(text))
+func TestDefaultSigner(t *testing.T) {
+	testVerify(t, DefaultSigner)
+}
 
-	if data, ok := DefaultSigner.verify(b); !ok {
+func TestCustomKeyShort(t *testing.T) {
+	var b [4]byte
+	rand.Read(b[:])
+	testVerify(t, NewSigner(b[:]))
+}
+
+func TestCustomKeyLong(t *testing.T) {
+	var b [200]byte
+	rand.Read(b[:])
+	testVerify(t, NewSigner(b[:]))
+}
+
+func testVerify(t *testing.T, signer *Signer) {
+	text := "hello"
+	b := signer.sign([]byte(text))
+
+	if data, ok := signer.verify(string(b)); !ok {
 		t.Errorf("Failed verify on unmodified value.")
 	} else if string(data) != text {
 		t.Errorf("Failed verify getting text. " + string(data))
 	}
 
 	b = append([]byte("test"), b[:len(b)-32]...)
-	if _, ok := DefaultSigner.verify(b); ok {
+	if _, ok := signer.verify(string(b)); ok {
 		t.Errorf("Failed verify on modified value.")
 	}
 
-	fakeHash := sha256.Sum256(b)
-	b = append([]byte("hello"), fakeHash[:]...)
-	if _, ok := DefaultSigner.verify(b); ok {
+	fakehash := sha256.Sum256(b)
+	s := base64.URLEncoding.EncodeToString(fakehash[:])
+	b = append(b, []byte(s)...)
+	if _, ok := signer.verify(string(b)); ok {
 		t.Errorf("Failed verify on fake hash.")
 	}
 }
